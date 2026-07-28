6528736db1aa019dbf277300727d555587c96c3d
Sameera Andraju <sameeraandraju31@gmail.com>
2026-07-28 19:41:40 +0530

    Add files via upload

---
+++ DeepSkilling/Module 05/Lab 3 EF Core/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace RetailInventoryLab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EF Core Migration Created Successfully");
+            Console.WriteLine("Database Updated Successfully");
+        }
+    }
+}
