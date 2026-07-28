b8ebcd5e5c10af6cb19386684d6c4cb93e27390e
Sameera Andraju <sameeraandraju31@gmail.com>
2026-07-28 20:50:01 +0530

    Add files via upload

---
+++ DeepSkilling/Module 08/student-course-portal/src/app/services/course.spec.ts
@@ -0,0 +1,22 @@
+import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+
+import { CourseService } from './course';
+
+describe('CourseService', () => {
+  let service: CourseService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient()
+      ]
+    });
+
+    service = TestBed.inject(CourseService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
---
+++ DeepSkilling/Module 08/student-course-portal/src/app/services/course.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
+
+  private apiUrl = 'http://localhost:3000/courses';
+
+  constructor(private http: HttpClient) {}
+
+  getCourses(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  addCourse(course: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, course);
+  }
+
+  updateCourse(id: string, course: any) {
+  return this.http.put(`${this.apiUrl}/${id}`, course);
+}
+
+ deleteCourse(id: string) {
+  return this.http.delete(`${this.apiUrl}/${id}`);
+}
+
+}
