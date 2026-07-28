bd484209d5c1339a2748ee12eb65962968db320b
Sameera Andraju <sameeraandraju31@gmail.com>
2026-07-28 20:05:50 +0530

    Add files via upload

---
+++ DeepSkilling/Module 06/WebAPI-HOL2/Models/Employee.cs
@@ -0,0 +1,11 @@
+namespace WebApiLab2.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Department { get; set; }
+    }
+}
